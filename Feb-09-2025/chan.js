c917b9de9025e291e474cfcd9404da55ec934c2e
Aziz2991 <azizullahaziz7777@gmail.com>
2025-02-12 13:19:25 -0800

    up

---
+++ Feb-09-2025/chan.js
@@ -0,0 +1,25 @@
+// let fullName = 'ghous';
+// var firstChar = fullName.slice(0,1).toUpperCase();
+// var result = firstChar + fullName.slice(1);
+// console.log(result)
+// var text = 'My name is Ghous Ahmed';
+// for(var i = 0; i<text.length; i ++){console.log(text.slice(i,i + 5) === 'Ahmed');}
+// var randomValues = 'ABCD3#434sdghasP3$43wgrebGsfs)(SFGFUSD*8$$43834'
+// for(var i = 0;i <10; i ++){
+//     var randomNumber =Math.round(Math.random() * randomValues.length);
+//     console.log(randomValues[randomNumber])
+// }
+// var date = new Date();
+// console.log(date.getDay())
+// console.log(typeof(date))
+// var dayName = ['sun','mon','tuesday','wednesday','thursday','friday','saturday']
+// console.log(dayName[date.getDay()])
+// var date = new Date();
+// console.log(date.getFullYear())
+// var date = new Date('9 September , 1992')
+//  var dayName = ['sun','mon','tuesday','wednesday','thursday','friday','saturday']
+//  console.log(dayName[
+let date = new Date();
+console.log(date);
+console.log(date.setFullYear(2000))
+console.log(date.setMonth(6))
---
+++ Feb-12-2025/app.js
@@ -0,0 +1,77 @@
+// let student = ['aziz','nasir','abid']
+// student[3] = 'hamid'
+// student[2] = 'zamir'
+// student[2] = 'ishaq'
+// console.log(student[2])
+// console.log(student.length);
+// let nam = ['hashir','jamal']
+// nam[10] = 'abc'
+// console.log(nam)
+
+// var num = ['hadi','umair','hashir']
+// num.pop();
+// let reuurn = num.pop()
+// console.log(reuurn)
+// console.log(num);
+
+// var num = ['hadi','umair','hashir']
+// num.push('anees','klsoom','zree','frzana')
+// var returnn = num.push('anees','klsoom','zree','frzana')
+// console.log(returnn)
+// console.log(num)
+// var num = ['hadi','umair','hashir']
+// num.shift();
+// var remain = num.shift()
+// console.log(num)
+// var num = ['hadi','umair','hashir','knvl','junaid']
+// var returnn = num.splice(1)
+// console.log(returnn)
+// console.log(num)
+// var num = ['hadi','umair','hashir','knvl','junaid']
+//  var returnn = num.slice(0,3)
+//  console.log(returnn)
+// console.log(num)
+
+// let studentName = ['Alice','Bob','Charlie']
+//  let n = studentName.push('umer')
+//  console.log(n)
+
+// console.log(studentName)
+// let studentName = ['Alice','Bob','Charlie']
+// let y = studentName.pop()
+// console.log(y)
+//  var fruits =['mango','banana','apple']
+//  var newu = fruits.shift()
+//  console.log(newu)
+
+//  var fruits =['mango','banana','apple']
+//  var l = fruits.unshift('orange','guava','carrot')
+// console.log(l)
+//  console.log(fruits)
+// var colors = ['red','blue','green','yellow']
+// colors.splice(1,2)
+// colors.splice(1,0,'purple','orange')
+
+// // console.log(colors)
+// console.log(ui)var numbers = [10,20,30,40,50,60]
+// var ui = numbers.slice(2,5)
+// var food = ['burger','pizza','pasta','salad']
+// food.push('Fries');
+// console.log(food)
+// food.shift(0,1)
+// console.log(food);
+// food.splice(2,1,'pasta')
+// console.log(food);
+// var n = food.slice(1,3)
+// console.log(n)
+var number = [5,10,15,20,25,30,35]
+let n = number.slice(2,5)
+console.log(n)
+console.log(number)
+
+var color = ['red','blue','green','yellow','purple','orange'];
+let u = color.slice(3)
+console.log(u)
+var animals = ['dog','cat','elephant','tiger','lion','girafee']
+var ny = animals.slice(0,3)
+console.log(ny)
